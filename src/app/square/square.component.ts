318eb7a81b2dcb6bd6e15265d2183fbec216af62
Dylan de Bruijn <dylan@bendingtherules.nl>
2023-01-03 11:24:41 +0100

    Project setup

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: ` <p>{{ rando }}</p> `,
+  styles: [],
+})
+export class SquareComponent {
+  rando = Math.random();
+}
